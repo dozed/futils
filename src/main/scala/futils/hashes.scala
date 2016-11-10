7c45b4a75fa4191b72851506990a5b09c26efeca
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2016-11-10 13:50:49 +0100

    Hashes always invoke getInstance

--- src/main/scala/futils/hashes.scala
+++ src/main/scala/futils/hashes.scala
@@ -3,18 +3,15 @@ package futils
 import java.security.MessageDigest
 
 object hashes {
-  private val sha = MessageDigest.getInstance("SHA-256")
-  private val md = MessageDigest.getInstance("MD5")
-
   def sha256(s: String): String = {
-    sha.digest(s.getBytes)
+    MessageDigest.getInstance("SHA-256").digest(s.getBytes)
       .foldLeft("")((s: String, b: Byte) => s +
         Character.forDigit((b & 0xf0) >> 4, 16) +
         Character.forDigit(b & 0x0f, 16))
   }
 
   def md5(s: String): String = {
-    md.digest(s.getBytes("UTF-8"))
+    MessageDigest.getInstance("MD5").digest(s.getBytes("UTF-8"))
       .foldLeft("")((s: String, b: Byte) => s +
         Character.forDigit((b & 0xf0) >> 4, 16) +
         Character.forDigit(b & 0x0f, 16))
